630c19ea916c745a4c6ba488fc83cbe536832bc1
Surajit Matia <surajitmatia2001@gmail.com>
2024-08-06 16:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
-let navbar = document.querySelector('.navbar')
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
-    searchForm.classList.remove('active');
+function myFun(){
+    let searchVal = document.getElementById("inp").value.toUpperCase() ;
+    let table = document.getElementsByTagName("table")
+    let tr = document.getElementsByTagName("tr");
+    let front = document.querySelector(".main")
+    for(let i=0; i<tr.length; i++){
+        let td = tr[i].getElementsByTagName("td")[0];
+        if(td){
+            let text = td.innerText;
+            if(text.toLocaleUpperCase().indexOf(searchVal) > -1){
+                tr[i].style.display="";
+                front.style.display="none"
+            }else{
+                tr[i].style.display="none"
+            }
+        }
+    }
+    if(searchVal == ""){
+        front.style.display="flex"
+    }
+    console.log(searchVal);
 }
- 
 
-let searchForm = document.querySelector('.search-form')
+function nav(){
+    let nav = document.querySelector(".nav");
+    nav.style.display="block"
+    let login = document.querySelector(".login");
+    login.style.display="none";
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-    searchForm.classList.remove('active');
+function login(){
+    let login = document.querySelector(".login");
+    login.style.display="block";
+
+    let nav = document.querySelector(".nav");
+    nav.style.display="none"
 }
- 
-var swiper = new Swiper(".review-slider", {
-    loop:true,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 5500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+
+
+let slide = document.querySelectorAll(".customer");
+let count = 0;
+console.log(slide);
+
+slide.forEach(function(customer, index){
+    customer.style.left=`${index * 100}%`
+})
+
+function next(){
+    count ++;
+    if(count == slide.length){
+        count=0;
+    }
+    bar()
+}
+
+function pre(){
+    count--;
+    if(count == -1){
+        count = slide.length-1
+    }
+    bar()
+}
+
+
+function bar(){
+    slide.forEach(function(customer){
+        customer.style.transform = `translateX(-${count * 100}%)`
+    })
+}
